35b6e97ee45cc6fe0e548b3bad55d4b1bda7b11a
andrew <booleanorama@gmail.com>
2025-02-27 00:22:47 -0500

    display jgp

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import ProductPrice from "./product-price";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
@@ -13,6 +14,7 @@ const ProductCard = ({ product }: { product: any }) => {
             height={300}
             width={300}
             priority={true}
+            unoptimized={true}
           />
         </Link>
       </CardHeader>
@@ -22,8 +24,9 @@ const ProductCard = ({ product }: { product: any }) => {
           <h2 className="text-sm font-medium">{product.name}</h2>
         </Link>
         <div className="flex-between gap-4">
+          <p>{product.rating} Stars</p>
           {product.stock > 0 ? (
-            <p className="font-bold">{product.price}</p>
+            <ProductPrice value={Number(product.price)} />
           ) : (
             <p className="text-destructive">Out Of Stock</p>
           )}
