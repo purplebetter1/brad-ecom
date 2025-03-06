9f9b0a08247cb1d1829e76c9a194283ee4542aee
andrew <booleanorama@gmail.com>
2025-03-05 22:07:03 -0500

    get data from server

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,8 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// Format number with decimal places
-export function formatNumberWithDecimal(num: number): string {
-  const [int, decimal] = num.toString().split(".");
-  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
+// Convert Prisma Object to regular JS Object
+export function convertToPlainObject<T>(value: T): T {
+  return JSON.parse(JSON.stringify(value));
 }
