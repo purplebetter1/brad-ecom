490982bee8f6fd9bf6e7528e98870a2abcf5006c
andrew <booleanorama@gmail.com>
2025-03-05 22:49:06 -0500

    add validators to insert product

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,8 @@
 import { z } from "zod";
-
 import { insertProductSchema } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: string;
-  numReviews: number;
   createdAt: Date;
 };
