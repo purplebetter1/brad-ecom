1f0a17b393d371a54ccb64535909643e54617fbf
andrew <booleanorama@gmail.com>
2025-04-12 21:00:58 -0400

    add order action and forms

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@ import {
   signInFormSchema,
   signUpFormSchema,
   shippingAddressSchema,
+  paymentMethodSchema,
 } from "../validators";
 import { signIn, signOut, auth } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
@@ -10,6 +11,8 @@ import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
+import { z } from "zod";
+import { getMyCart } from "./cart.actions";
 
 // Sign in user with credentials
 export async function signInWithCredentials(
@@ -34,6 +37,15 @@ export async function signInWithCredentials(
 
 // Sign user out
 export async function signOutUser() {
+  // get current user's cart and delete it so another user can sign in
+  const currentCart = await getMyCart();
+
+  if (currentCart?.id) {
+    await prisma.cart.delete({ where: { id: currentCart.id } });
+  } else {
+    console.log("No cart to delete");
+  }
+
   await signOut();
 }
 
@@ -109,5 +121,33 @@ export async function updateUserAddress(data: ShippingAddress) {
       success: false,
       message: formatError(error),
     };
+  }
+}
+
+// Update user's payment method
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+
+    if (!currentUser) throw new Error("User not found");
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+
+    return {
+      success: true,
+      message: "User updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
   }
 }
