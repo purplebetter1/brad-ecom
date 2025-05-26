2a2b2ac5ab6a570da2c7058805473fcfe025a8eb
andrew <booleanorama@gmail.com>
2025-05-26 12:24:54 -0400

    add product carousel

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,19 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
 import Menu from "./menu";
+import CategoryDrawer from "./category-drawer";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className="flex-start">
+          <Link href="/" className="flex-start ml-4">
+            <CategoryDrawer />
             <Image
               src="/images/logo.svg"
               alt={`${APP_NAME} logo`}
               height={48}
               width={48}
               priority={true}
+              className="ml-2"
             />
             <span className="hidden lg:block font-bold text-2xl ml-3">
               {APP_NAME}
