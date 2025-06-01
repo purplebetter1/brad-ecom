2fa48902639a65bdc0d119a2412687b66561a78b
andrew <booleanorama@gmail.com>
2025-06-01 11:59:51 -0400

    search page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -38,11 +38,20 @@ export async function getAllProducts({
   query,
   limit = PAGE_SIZE,
   page,
+  category,
+  price,
+  rating,
+  sort,
 }: {
   query: string;
   limit?: number;
   page: number;
+  category?: string;
+  price?: string;
+  rating?: string;
+  sort?: string;
 }) {
+  // Query filter
   const queryFilter: Prisma.ProductWhereInput =
     query && query !== "all"
       ? {
@@ -53,11 +62,45 @@ export async function getAllProducts({
         }
       : {};
 
+  // Category Filter
+  const categoryFilter = category && category !== "all" ? { category } : {};
+
+  // Price Filter
+  const priceFilter: Prisma.ProductWhereInput =
+    price && price !== "all"
+      ? {
+          price: {
+            gte: Number(price.split("-")[0]),
+            lte: Number(price.split("-")[1]),
+          },
+        }
+      : {};
+
+  //Rating Filter
+  const ratingFilter =
+    rating && rating !== "all"
+      ? {
+          rating: {
+            gte: Number(rating),
+          },
+        }
+      : {};
+
   const data = await prisma.product.findMany({
     where: {
       ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
     },
-    orderBy: { createdAt: "desc" },
+    orderBy:
+      sort === "lowest"
+        ? { price: "asc" }
+        : sort === "highest"
+        ? { price: "desc" }
+        : sort == "rating"
+        ? { rating: "desc" }
+        : { createdAt: "desc" },
     skip: (page - 1) * limit,
     take: limit,
   });
