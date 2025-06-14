3f173ab239bcf7a7a8bcfb8d7fed9e944529a952
andrew <booleanorama@gmail.com>
2025-06-13 21:47:01 -0400

    new fix for promise

--- app/(root)/order/[id]/stripe-payment-success/page.tsx
+++ app/(root)/order/[id]/stripe-payment-success/page.tsx
@@ -1,8 +1,8 @@
-import Stripe from "stripe";
-import { getOrderById } from "@/lib/actions/order.actions";
-import { notFound, redirect } from "next/navigation";
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { getOrderById } from "@/lib/actions/order.actions";
+import Link from "next/link";
+import { notFound, redirect } from "next/navigation";
+import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
@@ -14,7 +14,7 @@ const SuccessPage = async (props: {
   const { payment_intent: paymentIntentId } = await props.searchParams;
 
   // Fetch order
-  const order = getOrderById(id);
+  const order = await getOrderById(id);
   if (!order) notFound();
 
   // Retrieve payment intent
@@ -30,13 +30,14 @@ const SuccessPage = async (props: {
 
   // Check if payment is successful
   const isSuccess = paymentIntent.status === "succeeded";
+
   if (!isSuccess) return redirect(`/order/${id}`);
 
   return (
-    <div className="max-2-4xl w-full mx-auto space-y-8">
-      <div className="flex flex-col gap-6 items">
+    <div className="max-w-4xl w-full mx-auto space-y-8">
+      <div className="flex flex-col gap-6 items-center">
         <h1 className="h1-bold">Thanks for your purchase</h1>
-        <div>We are processing your order</div>
+        <div>We are processing your order.</div>
         <Button asChild>
           <Link href={`/order/${id}`}>View Order</Link>
         </Button>
