aa104ebaf577501ef4c368f9512bea09eb2c9f54
andrew <booleanorama@gmail.com>
2025-06-20 14:44:22 -0400

    send email paypal

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -7,11 +7,12 @@ import { getUserById } from "./user.actions";
 import { insertOrderSchema } from "../validators";
 
 import { prisma } from "@/db/prisma";
-import { CartItem, PaymentResult } from "@/types";
+import { CartItem, PaymentResult, ShippingAddress } from "@/types";
 import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
 import { PAGE_SIZE } from "../constants";
 import { Prisma } from "@prisma/client";
+import { sendPurchaseReceipt } from "@/email";
 
 // Create Order and Order Items
 export async function createOrder() {
@@ -243,6 +244,14 @@ export async function updateOrderToPaid({
     },
   });
   if (!updatedOrder) throw new Error("Order not found");
+
+  sendPurchaseReceipt({
+    order: {
+      ...updatedOrder,
+      shippingAddress: updatedOrder.shippingAddress as ShippingAddress,
+      paymentResult: updatedOrder.paymentResult as PaymentResult,
+    },
+  });
 }
 
 // Get user's orders
